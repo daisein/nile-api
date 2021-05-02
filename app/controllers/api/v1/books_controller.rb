28c33129aa5c7482a01ad78e8455d4166b96512b
Dai Li <dai.thomas.li3@gmail.com>
2021-05-02 16:11:57 +0800

    first commit

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,35 @@
+class Api::V1::BooksController < ApplicationController
+
+  def index
+    render json: Book.all
+  end
+
+
+  def create
+    # some logic
+    book = Book.new(book_params)
+    #book = Book.new(title: params[:title], author:params[:author])
+
+    if book.save
+      render json: book, status: :created
+    else
+      render json: book.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    Book.find(params[:id]).destroy!
+    head :no_content
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author)
+  end
+
+  def not_destroyed
+    render json:{}, status: :unprocessable_entity
+  end
+
+end
